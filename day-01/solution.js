73fef9aff5327710cd9bb2efd0f4e4b042bb7427
Rubén Chavarría <rchavarria@easyvista.com>
2025-12-07 11:57:39 +0100

    Count dial at 0 when rotating to the right

--- day-01/solution.js
+++ day-01/solution.js
@@ -7,13 +7,13 @@ export function readLines() {
 }
 
 function computeDialPosition(instruction, currentPosition) {
-  const direction = instruction[0];
-  const value = parseInt(instruction.slice(1), 10) % 100;
+  const instructionDirection = instruction[0];
+  const instructionValue = parseInt(instruction.slice(1), 10) % 100;
 
-  if (direction === 'L') {
-    return (currentPosition - value + 100) % 100;
-  } else if (direction === 'R') {
-    return (currentPosition + value) % 100;
+  if (instructionDirection === 'L') {
+    return (currentPosition - instructionValue + 100) % 100;
+  } else if (instructionDirection === 'R') {
+    return (currentPosition + instructionValue) % 100;
   }
 
   throw new Error(`Invalid instruction: ${instruction}`);
@@ -24,14 +24,34 @@ function processDialInstructions(instructions, position = 50, zeroCount = 0) {
     return zeroCount;
   }
 
-  const instr = instructions[0];
-  const computedPosition = computeDialPosition(instr, position);
+  const instruction = instructions[0];
+  const instructionDirection = instruction[0];
+  let instructionValue = parseInt(instruction.slice(1), 10);
+
+  // Count full rotations, increment zeroCount accordingly
+  //if (instructionValue >= 100) {
+  //  zeroCount += Math.floor(instructionValue / 100);
+  //  instructionValue = instructionValue % 100;
+  //}
+
+  let computedPosition = position;
+  if (instructionDirection === 'L') {
+    computedPosition = (position - instructionValue + 100) % 100;
+  } else if (instructionDirection === 'R') {
+    computedPosition = position + instructionValue;
+  }
+
+  if (computedPosition >= 100) {
+    zeroCount += Math.floor(computedPosition / 100);
+    computedPosition = computedPosition % 100;
+  }
+
   if (computedPosition === 0) {
     zeroCount++;
   }
 
   const prevStr = `D${position.toString().padStart(2, '0')}`;
-  const instrStr = instr.padEnd(3, ' ');
+  const instrStr = instruction.padEnd(3, ' ');
   const currStr = `D${computedPosition.toString().padStart(2, '0')}`;
 
   console.log(`${prevStr} => ${instrStr} => ${currStr}`);
@@ -39,6 +59,7 @@ function processDialInstructions(instructions, position = 50, zeroCount = 0) {
   return processDialInstructions(instructions.slice(1), computedPosition, zeroCount);
 }
 
-const instructions = readLines();
+//const instructions = readLines();
+const instructions = [ 'R151' ];
 const zeroCount = processDialInstructions(instructions);
 console.log(`Times dial was at position 0: ${zeroCount}`);
