ac1d66fd2812c8fc06467a9c8219a2873e639ebe
Rubén Chavarría <rchavarria@easyvista.com>
2025-12-09 08:40:17 +0100

    Parse ranges

--- day-02/solution.js
+++ day-02/solution.js
@@ -3,7 +3,13 @@ import path from 'path';
 
 function readRanges() {
   const filePath = path.join(path.dirname(new URL(import.meta.url).pathname), 'input.txt');
-  return fs.readFileSync(filePath, 'utf8').split(',');
+  return fs
+    .readFileSync(filePath, 'utf8')
+    .split(',')
+    .map(range => {
+      const [lower, upper] = range.split('-').map(Number);
+      return { lower, upper };
+    })
 }
 
-console.log('ranges:', readRanges());
+console.log('ranges:', readRanges())
