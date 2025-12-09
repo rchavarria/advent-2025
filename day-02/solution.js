d310d3794d3f0b1cdb5716ae4b1339700cab5b16
Rubén Chavarría <rchavarria@easyvista.com>
2025-12-09 08:44:40 +0100

    Find invalid ids

--- day-02/solution.js
+++ day-02/solution.js
@@ -12,4 +12,20 @@ function readRanges() {
     })
 }
 
-console.log('ranges:', readRanges())
+// console.log('ranges:', readRanges())
+
+function scanRange(range) {
+  for (let i = range.lower; i <= range.upper; i++) {
+    const str = i.toString();
+    if (str.length % 2 === 0) {
+      const half = str.length / 2;
+      if (str.slice(0, half) === str.slice(half)) {
+        console.log(i);
+      }
+    }
+  }
+}
+
+scanRange({ lower: 1, upper: 14 });
+console.log('---');
+scanRange({ lower: 16, upper: 35 });
