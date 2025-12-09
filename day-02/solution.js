f37c667ad5e6eacb51ce67ce5a75f1552a3ded06
Rubén Chavarría <rchavarria@easyvista.com>
2025-12-09 08:38:13 +0100

    Read ranges from file

---
+++ day-02/solution.js
@@ -0,0 +1,9 @@
+import fs from 'fs';
+import path from 'path';
+
+function readRanges() {
+  const filePath = path.join(path.dirname(new URL(import.meta.url).pathname), 'input.txt');
+  return fs.readFileSync(filePath, 'utf8').split(',');
+}
+
+console.log('ranges:', readRanges());
