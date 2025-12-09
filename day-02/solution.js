52034affacc7ce9bba6294c200e7aed69f9f5fa0
Rubén Chavarría <rchavarria@easyvista.com>
2025-12-09 11:05:59 +0100

    Clean code

--- day-02/solution.js
+++ day-02/solution.js
@@ -14,51 +14,20 @@ function readRanges() {
     })
 }
 
-function scanRange(range) {
-  const invalidIds = [];
-
-  for (let i = range.lower; i <= range.upper; i++) {
-    const id = i.toString();
-    if (id.length % 2 !== 0) {
-      continue;
-    }
-
-    const half = id.length / 2;
-    if (id.slice(0, half) !== id.slice(half)) {
-      continue;
-    }
-
-    invalidIds.push(i);
-  }
-
-  return invalidIds;
-}
-
 function isInvalidId(id) {
   return INVALID_ID_REGEXP.test(id.toString());
 }
 
-function newScanRange(range) {
+function scanRange(range) {
   return Array
     .from({ length: range.upper - range.lower + 1 }, (_, idx) => range.lower + idx)
     .filter(isInvalidId);
 }
 
-function print(id) {
-  console.log('Invalid:', id);
-  return id;
-}
-
 const sumInvalidIds = readRanges()
-// const sumInvalidIds = [
-//   { lower: 90, upper: 120 },
-// ]
-  .map(newScanRange)
-  // .map(scanRange)
+  .map(scanRange)
   .flat()
-  // .map(print)
   .reduce((acc, id) => acc + id, 0)
 
 console.log('Sum:', sumInvalidIds);
-
 console.assert(sumInvalidIds === 25663320831, 'Sum should be 25663320831');
