d7f64725cad2314b1432b109960be52644f9c979
Rubén Chavarría <rchavarria@easyvista.com>
2025-12-09 17:12:08 +0100

    Read ranges and ingredients

--- day-05/solution.js
+++ day-05/solution.js
@@ -1,9 +1,32 @@
 import fs from 'fs';
 import path from 'path';
 
-function readGrid() {
+function parseRange(rangeStr) {
+  const [min, max] = rangeStr.split('-').map(Number);
+  return { min, max };
+}
+
+function readRanges() {
   const filePath = path.join(path.dirname(new URL(import.meta.url).pathname), 'input.txt');
   return fs
     .readFileSync(filePath, 'utf8')
     .split('\r\n')
+    .filter(line => line.includes('-'))
+    .map(parseRange)
 }
+
+function readIngredients() {
+  const filePath = path.join(path.dirname(new URL(import.meta.url).pathname), 'input.txt');
+  return fs
+    .readFileSync(filePath, 'utf8')
+    .split('\r\n')
+    .filter(line => !line.includes('-'))
+    .filter(line => line.length > 0)
+    .map(Number)
+}
+
+const ranges = readRanges()
+const ingredients = readIngredients()
+
+console.log('ranges:', ranges)
+console.log('ingredients:', ingredients)
