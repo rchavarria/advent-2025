4372e89e394e44d8da17e84dc287b90651185c6f
Rubén Chavarría <rchavarria@easyvista.com>
2025-12-10 13:31:34 +0100

    :sparkles: Start working on Day 7

---
+++ day-07/solution.js
@@ -0,0 +1,11 @@
+import fs from 'fs';
+import path from 'path';
+
+function readInput() {
+  const filePath = path.join(path.dirname(new URL(import.meta.url).pathname), 'input.txt');
+  return fs
+    .readFileSync(filePath, 'utf8')
+    .split('\r\n')
+}
+
+console.log('Input lines:', readInput())
