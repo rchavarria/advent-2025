dc2455bdc2399eb77011d2e7cdd3ee77f1c02686
Rubén Chavarría <rchavarria@easyvista.com>
2025-12-11 15:45:35 +0100

    Designing class to represent junction boxes

--- day-08/solution.js
+++ day-08/solution.js
@@ -9,3 +9,23 @@ function readInput() {
 }
 
 console.log(readInput())
+
+export class Junction {
+  static from(coordinates) {
+    const [x, y, z] = coordinates.split(',').map(Number);
+    return new Junction(x, y, z);
+  }
+
+  constructor(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  distance(target) {
+    const dx = this.x - target.x;
+    const dy = this.y - target.y;
+    const dz = this.z - target.z;
+    return Math.sqrt(dx * dx + dy * dy + dz * dz);
+  }
+}
