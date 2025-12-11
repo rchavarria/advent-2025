4b88fd599b485791766f2a01d58a3b2aa5211f02
Rubén Chavarría <rchavarria@easyvista.com>
2025-12-11 16:48:08 +0100

    Junctions in a junction network (pair) might be in a different circuit each

--- day-08/tests.js
+++ day-08/tests.js
@@ -33,18 +33,33 @@ const sorted = networks
 const circuits = [];
 
 sorted.forEach(network => {
-  const foundCircuit = circuits.find(circuit => circuit.contains(network));
+  // Find all circuits that contain either junction of the network
+  const foundCircuits = circuits.filter(circuit => circuit.contains(network));
 
-  if (!foundCircuit) {
-    // Neither junction is in a circuit, create a new one
+  if (foundCircuits.length === 0) {
+    // No circuit contains either junction: create a new circuit
     const newCircuit = new Circuit();
     newCircuit.add(network.a);
     newCircuit.add(network.b);
     circuits.push(newCircuit);
+  } else if (foundCircuits.length === 1) {
+    // Only one circuit contains either junction: add both to that circuit
+    foundCircuits[0].add(network.a);
+    foundCircuits[0].add(network.b);
   } else {
-    // At least one junction is in a circuit, add both
-    foundCircuit.add(network.a);
-    foundCircuit.add(network.b);
+    // More than one circuit: merge them all and add both junctions
+    const mergedCircuit = new Circuit();
+    for (const circuit of foundCircuits) {
+      circuit.junctions.forEach(j => mergedCircuit.add(j));
+    }
+    mergedCircuit.add(network.a);
+    mergedCircuit.add(network.b);
+    // Remove all merged circuits from the list
+    for (const circuit of foundCircuits) {
+      const idx = circuits.indexOf(circuit);
+      if (idx !== -1) circuits.splice(idx, 1);
+    }
+    circuits.push(mergedCircuit);
   }
 });
 
